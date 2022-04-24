007148aebb289516d7da253af2ac0e740deeeac8
Paras Adhikari <paras.adhikari@stud.th-luebeck.de>
2022-04-24 13:49:39 +0200

    erste Maven template

---
+++ src/main/java/de/th_luebeck/swt2praktikum/Swt2praktikumApplication.java
@@ -0,0 +1,13 @@
+package de.th_luebeck.swt2praktikum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swt2praktikumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Swt2praktikumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/th_luebeck/swt2praktikum/Swt2praktikumApplicationTests.java
@@ -0,0 +1,13 @@
+package de.th_luebeck.swt2praktikum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Swt2praktikumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
