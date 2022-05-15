6cd7efb7cd4bfe102e0d2d4aa81c4d9cf9a14eb4
Nitesh <nitesh.bhattarai@stud.th-luebeck.de>
2022-05-15 20:18:52 +0200

    Registration Controller test update.

--- src/test/java/de/th_luebeck/swt2praktikum/controllers/registration/RegistrationControllerTest.java
+++ src/test/java/de/th_luebeck/swt2praktikum/controllers/registration/RegistrationControllerTest.java
@@ -1,21 +1,54 @@
-package de.th_luebeck.swt2praktikum.controllers.registration;
+package de.th_luebeck.swt2praktikum;
 
+import de.th_luebeck.swt2praktikum.controllers.UserController;
+import de.th_luebeck.swt2praktikum.controllers.registration.RegistrationController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
 
-class RegistrationControllerTest {
+import de.th_luebeck.swt2praktikum.controllers.registration.RegistrationInput;
+import de.th_luebeck.swt2praktikum.entities.User;
+import de.th_luebeck.swt2praktikum.repositories.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+
+@SpringBootTest
+public class RegistrationControllerTest {
+
 
     @Test
-    void registrationCheck() {
-
+    public void contextLoads() throws Exception {
     }
 
-    @Test
-    void registration() {
-    }
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RegistrationController registrationController;
 
     @Test
-    void testRegistration() {
+    public void registerTest() {
+
+        RegistrationInput registrationInput = new RegistrationInput("testname", "testusername",
+                "testPassword12@", "testPassword12@", "testEmail@example.com", true);
+
+
+        registrationController.registration(registrationInput);
+
+
+        User user = new User(registrationInput.getUserName(),
+                BCrypt.hashpw(registrationInput.getPassword(), BCrypt.gensalt()),
+                registrationInput.getEmailAddress(),registrationInput.getName());
+
+
+        User savedUser = userRepository.save(user);
+
+        assertEquals(registrationInput.getUserName(), savedUser.getUserName());
+
     }
 }
